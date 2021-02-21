58da9191e2ae1057c69c990c9bc2fb5336f3fb11
tahir <tahir@tahirs-MacBook-Pro.local>
2021-02-21 16:01:05 -0500

    Adding shortest trace algo

--- src/main/java/com/instana/assignment/graph/TraceGraph.java
+++ src/main/java/com/instana/assignment/graph/TraceGraph.java
@@ -6,10 +6,10 @@ import com.instana.assignment.model.Vertex;
 
 import java.util.*;
 
-@FunctionalInterface
-interface Filter {
-    boolean filter(Vertex p, Vertex to, int depth, int avgLatency, List<Vertex> cp);
-}
+//@FunctionalInterface
+//interface Filter {
+//    boolean filter(Vertex p, Vertex to, int depth, int avgLatency, List<Vertex> cp);
+//}
 
 public class TraceGraph {
     List<Vertex> nodes;
@@ -18,7 +18,7 @@ public class TraceGraph {
         this.nodes = nodes;
     }
 
-    public int avgLatency(Vertex... vertices) {
+    public int avgLatencyByHops(Vertex... vertices) {
         int avgLatency = 0;
 
         if (vertices.length > 0) {
@@ -50,183 +50,46 @@ public class TraceGraph {
         return avgLatency;
     }
 
-    public int avgLatency(List<Vertex> trace) {
-        Queue<Vertex> queue = new ArrayDeque();
-        queue.add(trace.get(0));
+    public int avgLatencyByTrace(Vertex source, Vertex dest) {
+        //Store the min distance from start node
+        Map<Vertex, Integer> dist = new HashMap<>();
+        //Store the immediate parent node from the start node
+        Map<Vertex, Vertex> cameFrom = new HashMap<>();
 
-        int next = 1;
-        int dist = 0;
-        while (!queue.isEmpty()) {
-            Vertex v = queue.poll();
-
-            boolean f = false;
-            for (Edge e : v.getEdges()) {
-                if (e.getTo() == trace.get(next)) {
-                    dist += e.getCost();
-                    next++;
-                    if (next == trace.size()) {
-                        f = true;
-                        break;
-                    }
-                    f = true;
-                    queue.add(e.getTo());
-                    break;
-                }
-            }
-
-            if (!f) {
-                System.out.println("JJJJJ");
-                return -1;
-            }
-
-        }
-//        Vertex start = trace.get(0);
-//        int next = 1;
-//        boolean found = false;
-//        int di = 0;
-//        for (Edge e : start.getEdges()) {
-//            if (e.getTo() == trace.get(next)) {
-//                found = true;
-//                di += e.getCost();
-//                next++;
-//            }
-//        }
-//        return 0;
-
-        return dist;
-    }
-
-    Map<Vertex, Map<Vertex, VertexInfo>> info = new HashMap<>();
-
-    public void run() {
-
-
-        for (int i = 0; i < nodes.size(); i++) {
-            for (int j = 0; j < nodes.size(); j++) {
-                if (i != j) {
-                    Vertex from = nodes.get(i);
-                    Vertex to = nodes.get(j);
-//                    buildAll(from, this.nodes.get(3));
-                    buildAll(from, to);
-//                    return;
-                }
-            }
-        }
-
-        System.out.println(info);
-    }
-
-    void deepTrace1(Vertex s, Vertex d) {
-        for (Edge e : s.getEdges()) {
-            List<Vertex> cp = new ArrayList<>();
-            cp.add(s);
-            expand1(e.getTo(), d, e.getCost(), cp);
-        }
-    }
-
-    void expand1(Vertex f, Vertex t, int sum, List<Vertex> cp) {
-        if (sum > 30) {
-            cp.remove(cp.size() - 1);
-            return;
-        }
-
-        cp.add(f);
-        if (f == t && sum < 30) {
-            System.out.println("*********");
-            for (Vertex v : cp) {
-                System.out.print(v.getValue() + ",");
-            }
-//            cp.remove(cp.size()-1);
-            System.out.println();
-        }
-
-        for (Edge e : f.getEdges()) {
-            expand1(e.getTo(), t, e.getCost() + sum, cp);
-        }
-
-    }
-
-    void deepTrace(Vertex s, Vertex d, Filter filter) {
-        int curr = 0;
-
-        for (Edge e : s.getEdges()) {
-            List<Vertex> cp = new ArrayList<>();
-            cp.add(s);
-            inflate(e.getTo(), d, 1, filter, e.getCost(), cp);
-            System.out.println("\n*******\n");
-        }
-        System.out.println(curr);
-
-    }
-
-
-    void inflate(Vertex parent, Vertex to, int depth, Filter filter, int sumAvg, List<Vertex> cp) {
-
-        cp.add(parent);
-        if (!filter.filter(parent, to, depth, sumAvg, cp)) {
-            return;
-        }
-
-
-        for (Edge e : parent.getEdges()) {
-            inflate(e.getTo(), to, depth + 1, filter, sumAvg + e.getCost(), cp);
-        }
-    }
-
-    void buildAll(Vertex s, Vertex d) {
-        List<Vertex> currentPath = new ArrayList<>();
-        List<List<Vertex>> allPaths = new ArrayList<>();
         Set<Vertex> visited = new HashSet<>();
+        //Always pick node which has the smallest distance from the start node
+        PriorityQueue<Vertex> pq = new PriorityQueue<>(Comparator.comparingInt(dist::get));
 
-        currentPath.add(s);
-        dfs(s, d, visited, currentPath, allPaths);
-        System.out.println("---->" + allPaths);
-
-//        VertexInfo ii = new VertexInfo();
-//        ii.path = allPaths;
-//
-//        if (!info.containsKey(s)) {
-//            info.put(s, new HashMap<>());
-//        }
-//        info.get(s).put(d, ii);
-
-    }
-
-    private void dfs(Vertex s, Vertex d, Set<Vertex> visited, List<Vertex> currentPath, List<List<Vertex>> allPaths) {
-
-        visited.add(s);
-        if (s.equals(d)) {
-            System.out.println(currentPath);
-
-            List<Vertex> st = new ArrayList<>();
-            for (Vertex v : currentPath) {
-                st.add(v);
-            }
-            allPaths.add(st);
-
-            visited.remove(s);
-            return;
+        for (Vertex v : this.nodes) {
+            dist.put(v, Integer.MAX_VALUE);
+            cameFrom.put(v, null);
         }
 
-        for (Edge e : s.getEdges()) {
-            Vertex to = e.getTo();
-            if (!visited.contains(to)) {
-                currentPath.add(to);
-                dfs(to, d, visited, currentPath, allPaths);
-                currentPath.remove(to);
-//                visited.clear();
-            } else {
-//                System.out.println("Already:::"+ to);
+        for (Edge e : source.getEdges()) {
+            pq.add(e.getTo());
+            dist.put(e.getTo(), e.getCost());
+            cameFrom.put(e.getTo(), source);
+        }
+        while (!pq.isEmpty()) {
+            Vertex current = pq.poll();
+//            if (current.equals(dest)) {
+//                break;
+//            }
+            visited.add(current);
+
+            for (Edge edge : current.getEdges()) {
+                Vertex next = edge.getTo();
+                if (!visited.contains(next)) {
+                    int newDist = dist.get(current) + edge.getCost();
+                    if (newDist < dist.get(next)) {
+                        dist.put(next, newDist);
+                        cameFrom.put(next, current);
+                    }
+                    pq.add(next);
+                }
             }
         }
+
+        return dist.get(dest);
     }
-
-    public List<Vertex> getNodes() {
-        return nodes;
-    }
-}
-
-class VertexInfo {
-    List<List<Vertex>> path;
-
 }
--- src/test/java/com/instana/assignment/graph/TraceGraphTest.java
+++ src/test/java/com/instana/assignment/graph/TraceGraphTest.java
@@ -1,17 +1,18 @@
 package com.instana.assignment.graph;
 
 import com.instana.assignment.exception.NoSuchTraceException;
-import com.instana.assignment.model.Edge;
 import com.instana.assignment.model.Vertex;
 import com.instana.assignment.utils.CsvReader;
 import com.instana.assignment.utils.GraphImporter;
 import com.instana.assignment.utils.InvalidInputException;
 import com.instana.assignment.utils.TraceInput;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.io.*;
-import java.util.*;
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TraceGraphTest {
     final String path = "src/test/resources/input.csv";
@@ -24,8 +25,8 @@ public class TraceGraphTest {
         Vertex vC = vertices.get(2);
 
         TraceGraph graph = new TraceGraph(vertices);
-        int latency = graph.avgLatency(vA, vB, vC);
-        Assertions.assertEquals(9, latency);
+        int latency = graph.avgLatencyByHops(vA, vB, vC);
+        assertEquals(9, latency);
     }
 
     @Test
@@ -35,8 +36,8 @@ public class TraceGraphTest {
         Vertex vD = vertices.get(3);
 
         TraceGraph graph = new TraceGraph(vertices);
-        int latency = graph.avgLatency(vA, vD);
-        Assertions.assertEquals(5, latency);
+        int latency = graph.avgLatencyByHops(vA, vD);
+        assertEquals(5, latency);
     }
 
     @Test
@@ -47,8 +48,8 @@ public class TraceGraphTest {
         Vertex vD = vertices.get(3);
 
         TraceGraph graph = new TraceGraph(vertices);
-        int latency = graph.avgLatency(vA, vD, vC);
-        Assertions.assertEquals(13, latency);
+        int latency = graph.avgLatencyByHops(vA, vD, vC);
+        assertEquals(13, latency);
     }
 
     @Test
@@ -61,8 +62,8 @@ public class TraceGraphTest {
         Vertex vE = vertices.get(4);
 
         TraceGraph graph = new TraceGraph(vertices);
-        int latency = graph.avgLatency(vA, vE, vB, vC, vD);
-        Assertions.assertEquals(22, latency);
+        int latency = graph.avgLatencyByHops(vA, vE, vB, vC, vD);
+        assertEquals(22, latency);
     }
 
     @Test()
@@ -73,9 +74,28 @@ public class TraceGraphTest {
         Vertex vE = vertices.get(4);
 
         TraceGraph graph = new TraceGraph(vertices);
-        Assertions.assertThrows(NoSuchTraceException.class, ()-> graph.avgLatency(vA, vE, vD));
+        assertThrows(NoSuchTraceException.class, ()-> graph.avgLatencyByHops(vA, vE, vD));
     }
 
+    @Test()
+    public void avgLatencyShortestAC() throws IOException, InvalidInputException {
+        List<Vertex> vertices = importGraph();
+        Vertex vA = vertices.get(0);
+        Vertex vC = vertices.get(2);
+
+        TraceGraph graph = new TraceGraph(vertices);
+        assertEquals(9, graph.avgLatencyByTrace(vA, vC));
+    }
+
+    @Test()
+    public void avgLatencyShortestAB() throws IOException, InvalidInputException {
+        List<Vertex> vertices = importGraph();
+        Vertex vA = vertices.get(0);
+        Vertex vB = vertices.get(1);
+
+        TraceGraph graph = new TraceGraph(vertices);
+        assertEquals(9, graph.avgLatencyByTrace(vB, vB));
+    }
 
     private List<Vertex> importGraph() throws IOException, InvalidInputException {
         List<TraceInput> traceInputs = CsvReader.readFile(path);
