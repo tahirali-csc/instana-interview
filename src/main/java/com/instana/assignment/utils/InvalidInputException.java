32906c20b32ebcae2bd6b87ac70da804728032f6
tahir <tahir@tahirs-MacBook-Pro.local>
2021-02-22 10:00:02 -0500

    Updating docs

--- src/main/java/com/instana/assignment/utils/InvalidInputException.java
+++ src/main/java/com/instana/assignment/utils/InvalidInputException.java
@@ -1,5 +1,8 @@
 package com.instana.assignment.utils;
 
+/**
+ * Represents an invalid input exception
+ */
 public class InvalidInputException extends Exception {
     public InvalidInputException(String msg) {
         super(msg);
